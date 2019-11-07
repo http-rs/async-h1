d078b74ee8191391728f7bde6f05ac16bdcb2ad4
Ryan Levick <ryan.levick@gmail.com>
2019-11-07 15:07:42 +0100

    Small clean up

--- examples/client.rs
+++ examples/client.rs
@@ -1,6 +1,5 @@
 use async_h1::{client, Body};
 use async_std::{io, net, task};
-use http::Response;
 
 fn main() -> Result<(), async_h1::Exception> {
     task::block_on(async {
@@ -9,8 +8,8 @@ fn main() -> Result<(), async_h1::Exception> {
         let body = Body::from_string("hello chashu".to_owned());
         let mut req = client::encode(http::Request::new(body)).await?;
         io::copy(&mut req, writer).await?;
-        let res: Response<Body<io::BufReader<&mut &async_std::net::TcpStream>>> = client::decode(reader).await?;
+        let res = client::decode(reader).await?;
         println!("Response {:?}", res);
-        Ok::<(), async_h1::Exception>(())
+        Ok(())
     })
 }
