fda7ba95242f3cb2c961383e2682f602dcd03d54
Yoshua Wuyts <yoshuawuyts@gmail.com>
2019-11-12 19:11:30 +0100

    update example
    Signed-off-by: Yoshua Wuyts <yoshuawuyts@gmail.com>

--- examples/server.rs
+++ examples/server.rs
@@ -1,6 +1,7 @@
 use async_h1::{server, Body};
 use async_std::prelude::*;
-use async_std::{io, net, task};
+use async_std::net;
+use async_std::task;
 
 fn main() -> Result<(), async_h1::Exception> {
     task::block_on(async {
@@ -9,26 +10,12 @@ fn main() -> Result<(), async_h1::Exception> {
         let mut incoming = listener.incoming();
 
         while let Some(stream) = incoming.next().await {
-            // task::spawn(async move {
-            //     let stream = stream?;
-            //     let (reader, writer) = &mut (&stream, &stream);
-            //     let req = server::decode(reader).await?;
-            //     if let server::OptionalRequest::Request(req) = req {
-            //         // dbg!(req);
-            //         let body = Body::from_string("hello chashu".to_owned());
-            //         let mut res = server::encode(http::Response::new(body)).await?;
-            //         io::copy(&mut res, writer).await?;
-            //     }
-            //     Ok::<(), async_h1::Exception>(())
-            // });
             task::spawn(async {
                 let stream = stream?;
                 let (reader, writer) = &mut (&stream, &stream);
-                server::connect(reader, writer, |req| {
-                    async {
-                        let body = Body::from_string("hello chashu".to_owned());
-                        Ok(http::Response::new(body))
-                    }
+                server::connect(reader, writer, |_| async {
+                    let body = Body::from_string("hello chashu".to_owned());
+                    Ok(http::Response::new(body))
                 })
                 .await
             });
