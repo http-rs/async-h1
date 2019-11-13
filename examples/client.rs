c459613b977a01327f094d1f5720c97960cee2ec
dignifiedquire <me@dignifiedquire.com>
2019-11-13 21:07:44 +0100

    demonstrate keepalive in the client

--- examples/client.rs
+++ examples/client.rs
@@ -3,13 +3,22 @@ use async_std::{io, net, task};
 
 fn main() -> Result<(), async_h1::Exception> {
     task::block_on(async {
-        let stream = net::TcpStream::connect("127.0.0.1:8080").await?;
-        let (reader, writer) = &mut (&stream, &stream);
-        let body = Body::from_string("hello chashu".to_owned());
-        let mut req = client::encode(http::Request::new(body)).await?;
-        io::copy(&mut req, writer).await?;
-        let res = client::decode(reader).await?;
-        println!("Response {:?}", res);
+        let tcp_stream = net::TcpStream::connect("127.0.0.1:8080").await?;
+        println!("connecting to {}", tcp_stream.peer_addr()?);
+
+        for i in 0usize..2 {
+            let (tcp_reader, tcp_writer) = &mut (&tcp_stream, &tcp_stream);
+
+            println!("making request {}/2", i + 1);
+
+            let body = Body::from_string("hello chashu".to_owned());
+            let mut req = client::encode(http::Request::new(body)).await?;
+            io::copy(&mut req, tcp_writer).await?;
+
+            // read the response
+            let res = client::decode(tcp_reader).await?;
+            println!("{:?}", res);
+        }
         Ok(())
     })
 }
