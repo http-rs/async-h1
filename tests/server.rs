919b5e4bd915ac34c0389dfb91867dac07bd6bfe
Ryan Levick <ryan.levick@gmail.com>
2019-12-20 12:14:24 +0100

    Clean up tests

--- tests/server.rs
+++ tests/server.rs
@@ -1,26 +1,23 @@
 mod common;
 use async_h1::server;
 use async_std::task;
-use common::read_fixture;
+use common::TestCase;
 use http_types::{Response, StatusCode};
 
 #[test]
 fn test_basic_request() {
     task::block_on(async {
-        let io = read_fixture("request1").await;
-        let mut expected = read_fixture("response1").await;
+        let case = TestCase::new("request1", "response1").await;
         let addr = "http://example.com";
 
-        assert!(
-            io.clone(),
-            expected,
-            server::accept(addr, io.clone(), |_req| {
-                async {
-                    let mut resp = Response::new(StatusCode::Ok);
-                    resp.set_body("");
-                    Ok(resp)
-                }
-            })
-        );
-    })
+        server::accept(addr, case.clone(), |_req| async {
+            let mut resp = Response::new(StatusCode::Ok);
+            resp.set_body("");
+            Ok(resp)
+        })
+        .await
+        .unwrap();
+
+        case.assert().await;
+    });
 }
