f90b12d4778baae50644181b34bf921c4c48dee3
Yoshua Wuyts <yoshuawuyts@gmail.com>
2020-04-14 14:15:59 +0200

    revert examples

--- examples/server.rs
+++ examples/server.rs
@@ -1,9 +1,7 @@
-use async_std::fs::File;
-use async_std::io::BufReader;
 use async_std::net::{TcpListener, TcpStream};
 use async_std::prelude::*;
 use async_std::task;
-use http_types::{Body, Response, StatusCode};
+use http_types::{Response, StatusCode};
 
 #[async_std::main]
 async fn main() -> http_types::Result<()> {
@@ -32,11 +30,7 @@ async fn accept(addr: String, stream: TcpStream) -> http_types::Result<()> {
     async_h1::accept(&addr, stream.clone(), |_req| async move {
         let mut res = Response::new(StatusCode::Ok);
         res.insert_header("Content-Type", "text/plain")?;
-        let body = Body::from_reader(
-            BufReader::new(File::open("tests/fixtures/zeros.txt").await?),
-            None,
-        );
-        res.set_body(body);
+        res.set_body("Hello world");
         Ok(res)
     })
     .await?;
