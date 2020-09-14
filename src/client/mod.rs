0c2fa7b5b0a161bbc628cebcfc233417bffdfa3e
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2020-09-14 15:57:58 -0700

    client: add default user agent

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -1,7 +1,9 @@
 //! Process HTTP connections on the client.
 
 use async_std::io::{self, Read, Write};
+use http_types::headers::USER_AGENT;
 use http_types::{Request, Response};
+use lazy_static::lazy_static;
 
 mod decode;
 mod encode;
@@ -9,11 +11,19 @@ mod encode;
 pub use decode::decode;
 pub use encode::Encoder;
 
+lazy_static! {
+    static ref DEFAULT_USER_AGENT: String = format!("http-rs-h1/{}", env!("CARGO_PKG_VERSION"));
+}
+
 /// Opens an HTTP/1.1 connection to a remote host.
-pub async fn connect<RW>(mut stream: RW, req: Request) -> http_types::Result<Response>
+pub async fn connect<RW>(mut stream: RW, mut req: Request) -> http_types::Result<Response>
 where
     RW: Read + Write + Send + Sync + Unpin + 'static,
 {
+    if let None = req.header(USER_AGENT) {
+        req.insert_header(USER_AGENT, DEFAULT_USER_AGENT.as_str());
+    }
+
     let mut req = Encoder::encode(req).await?;
     log::trace!("> {:?}", &req);
 
