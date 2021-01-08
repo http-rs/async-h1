14aff92c8210bd1ef485f1f3f3ac76cede0819ef
mu2019 <sheng.2179@gmail.com>
2021-01-08 11:10:30 +0800

    fix fmt error.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -132,8 +132,7 @@ where
             .map(|connection| connection.as_str())
             .unwrap_or("");
 
-        let connection_header_is_upgrade = connection_header_as_str.split(',')
-            .any(|s| s.trim().eq_ignore_ascii_case("upgrade"));
+        let connection_header_is_upgrade = connection_header_as_str.split(',').any(|s| s.trim().eq_ignore_ascii_case("upgrade"));
         let mut close_connection = connection_header_as_str.eq_ignore_ascii_case("close");
 
         let upgrade_requested = has_upgrade_header && connection_header_is_upgrade;
